1146249c09c57434583d94c40d1c0184817c16d0
alexanderelmuratov <elmuratov@gmail.com>
2022-09-25 00:35:35 +0300

    Update StartButton.jsx

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,26 +1,7 @@
-import useSound from 'use-sound';
-import mySound from '../audio/kalinka.mp3';
-
 import { StyledStartButton } from './styles/StyledStartButton';
 
 const StartButton = ({ callback }) => {
-  // const [playSound, { stop }] = useSound(mySound, {
-  //   volume: 0.1,
-  //   loop: true,
-  // });
-  // console.log('playSound', playSound);
-
-  return (
-    <StyledStartButton
-      onClick={() => {
-        // stop();
-        callback();
-        // playSound();
-      }}
-    >
-      Start Game
-    </StyledStartButton>
-  );
+  return <StyledStartButton onClick={callback}>Start Game</StyledStartButton>;
 };
 
 export default StartButton;
