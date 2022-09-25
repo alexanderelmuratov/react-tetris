1a70067ad08b6416052a34daa35fd37effcbf0b9
alexanderelmuratov <elmuratov@gmail.com>
2022-09-25 10:41:08 +0300

    Update Stage.jsx

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -2,10 +2,13 @@ import Cell from './Cell';
 
 import { StyledStage } from './styles/StyledStage';
 
-const Stage = ({ stage }) => (
-  <StyledStage width={stage[0].length} height={stage.length}>
-    {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-  </StyledStage>
-);
+const Stage = ({ stage }) => {
+  console.log('stage', stage);
+  return (
+    <StyledStage width={stage[0].length} height={stage.length}>
+      {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+    </StyledStage>
+  );
+};
 
 export default Stage;
