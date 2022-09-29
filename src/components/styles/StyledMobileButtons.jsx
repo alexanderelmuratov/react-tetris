f88694d646a836f8553c21f85362ce82785a966a
alexanderelmuratov <elmuratov@gmail.com>
2022-09-29 22:17:56 +0300

    Update StyledMobileButtons.jsx

--- src/components/styles/StyledMobileButtons.jsx
+++ src/components/styles/StyledMobileButtons.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledMobileButtons = styled.div`
   display: flex;
   width: 70%;
-  justify-content: space-around;
+  justify-content: space-between;
   flex-wrap: wrap;
 
   button {
